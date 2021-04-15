df640537553da07908730f59092422ac72d73905
YuryLandau <yurylandau@gmail.com>
2021-04-14 23:15:25 -0300

    Teste arquivos estaticos

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 3000;
 //usará as rotas importadas.
 server.use(routes);
 
+server.use('/static', express.static('public'));
+
 server.listen(port, () => {
     console.log(`rodando liso na porta ${port}`)
 })
