539b83181245836824c380b09554263e7ca53b3d
YuryLandau <yurylandau@gmail.com>
2021-04-14 21:31:11 -0300

    trocando view engine

--- src/server.js
+++ src/server.js
@@ -3,12 +3,16 @@ const server = express();
 const routes = require('./routes')
 const port = 3000;
 
-server.listen(port, () => {
-    console.log(`rodando liso na porta ${port}`)
-})
+//Usando template engine
+server.set("view engine", "ejs");
+server.set('views', __dirname + '/views'); //informando para o ejs qual a pasta dos views.
 
 //habilitar arquivos estáticos
 server.use(express.static("public"));
 
 //usará as rotas importadas.
 server.use(routes);
+
+server.listen(port, () => {
+    console.log(`rodando liso na porta ${port}`)
+})
